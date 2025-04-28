61eb521de5b720d52afc154a4d80efeebc9fb12d
Kebikov Evgenii <kebikovgen@gmail.com>
2025-04-28 23:13:37 +0300

    added data for series weddings

--- src/data/weddingSeries/dataWeddingSeries.ts
+++ src/data/weddingSeries/dataWeddingSeries.ts
@@ -1,8 +1,18 @@
 import type { TDataWeddingSeries } from "./types";
 import { MY } from "./MY/dataSeriesMY";
+import { PT } from "./PT/dataSeriesPT";
+import { DA } from "./DA/dataSeriesDA";
+import { DK } from "./DK/dataSeriesDK";
+import { JD } from "./JD/dataSeriesJD";
+import { SD } from "./SD/dataSeriesSD";
+import { SY } from "./SY/dataSeriesSY";
+import { SL } from "./SL/dataSeriesSL";
+import { NK } from "./NK/dataSeriesNK";
+
 
 const dataWeddingSeries: TDataWeddingSeries = {
-    MY
+    MY, PT, DA, DK, JD, SD, SY, SL, NK
 }
+
 
 export default dataWeddingSeries;
