9095d62d6d8e2835a7d5179401f929dbef9cf0ce
Kebikov Evgenii <kebikovgen@gmail.com>
2025-05-11 23:45:40 +0300

    remade nextConfig: added trailingSlash

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,8 @@ const nextConfig: NextConfig = {
         formats: ['image/avif', 'image/webp'],
         deviceSizes: [128, 256, 384, 640, 750, 828, 1080, 1200, 1920, 2048, 2560, 3840]
     },
+     // предотврашяет редирект с /page на /page/
+    trailingSlash: false,
 
     async redirects() {
         return [
