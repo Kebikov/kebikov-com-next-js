93caa46ffc7b8ccfb2c98780b71d01194d090ce9
Kebikov Evgenii <kebikovgen@gmail.com>
2025-05-11 21:49:49 +0300

    added 301

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,25 @@ const nextConfig: NextConfig = {
         formats: ['image/avif', 'image/webp'],
         deviceSizes: [128, 256, 384, 640, 750, 828, 1080, 1200, 1920, 2048, 2560, 3840]
     },
+
+    async redirects() {
+        return [
+            { source: '/svadebnye-fotografii', destination: 'https://kebikov.com/weddings-all', permanent: true },
+            { source: '/wedding-slideshow', destination: 'https://kebikov.com/slideshow', permanent: true },
+            { source: '/stati', destination: 'https://kebikov.com/articles', permanent: true },
+            { source: '/obomne', destination: 'https://kebikov.com/about-me', permanent: true },
+            { source: '/otziviklientov', destination: 'https://kebikov.com/reviews', permanent: true },
+            { source: '/paketiuslug', destination: 'https://kebikov.com/price-packages', permanent: true },
+            { source: '/kontakt', destination: 'https://kebikov.com/contacts', permanent: true },
+
+            { source: '/15-variantov-krasivogo-svadebnogo-finala', destination: 'https://kebikov.com/articles/beautiful-wedding-finale', permanent: true },
+            { source: '/8-originalnyx-sposobov-brosit-buket-nevesty', destination: 'https://kebikov.com/articles/bouquet-roll', permanent: true },
+            { source: '/c-chego-nachat-podgotovku-k-svadbe', destination: 'https://kebikov.com/articles/preparations-wedding', permanent: true },
+            { source: '/7-sovetov-dlya-nevest-ot-fotografa', destination: 'https://kebikov.com/articles/from-the-photographer', permanent: true },
+            { source: '/perfect-wedding-photo-session', destination: 'https://kebikov.com/articles/perfect-wedding-photo-session', permanent: true },
+            { source: '/aksessuary-dlya-svadebnoj-fotosessii', destination: 'https://kebikov.com/articles/accessories', permanent: true }
+        ]
+    }
 };
 
 export default nextConfig;
